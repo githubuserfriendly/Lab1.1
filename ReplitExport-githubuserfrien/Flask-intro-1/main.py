ffa03a938146abaeae8b9bc05e5b12894365f4b5
AKhantf <122883444+githubuserfriendly@users.noreply.github.com>
2025-01-26 15:11:38 -0400

    Update main.py

--- ReplitExport-githubuserfrien/Flask-intro-1/main.py
+++ ReplitExport-githubuserfrien/Flask-intro-1/main.py
@@ -33,4 +33,34 @@ def get_student(id):
     if student['id'] == id: # filter out the students without the specified id
       return jsonify(student)
 
+@app.route('/stats')
+def get_stats():
+    chicken = 0
+    fish = 0
+    cs = 0
+    it = 0
+    cs_special = 0
+    it_special = 0
+    for student in data:
+        if student['pref'] == 'Chicken':
+            chicken += 1
+        elif student['pref'] == 'Fish':
+            fish += 1
+        if student['programme'] == 'Computer Science (Special)':
+            cs_special += 1
+        elif student['programme'] == 'Information Technology (Special)':
+            it_special += 1
+        if student['programme'] == 'Computer Science (Major)':
+            cs += 1
+        elif student['programme'] == 'Information Technology (Major)':
+            it += 1
+    return jsonify({
+        'Chicken': chicken,
+        'Fish': fish,
+        'Computer Science (Special)': cs_special,
+        'Information Technology (Special)': it_special,
+        'Computer Science': cs,
+        'Information Technology': it
+    })
+
 app.run(host='0.0.0.0', port=8080, debug=True)
